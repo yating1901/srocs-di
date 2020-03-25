9d164244c3e4cf52f4d7ef2b404a84dba2f755a7
Michael Allwright <allsey87@gmail.com>
2020-03-25 16:19:58 +0100

    Added a condition-action framework to the loop functions

--- src/loop_functions/di_srocs_loop_functions.cpp
+++ src/loop_functions/di_srocs_loop_functions.cpp
@@ -8,112 +8,250 @@ namespace argos {
    /****************************************/
    /****************************************/
 
-   CDISRoCSLoopFunctions::CDISRoCSLoopFunctions() :
-      m_cSpace(CSimulator::GetInstance().GetSpace()),
-      m_cArenaRangeX(-1.0,1.0),
-      m_cArenaRangeY(-1.0,1.0) {
-      /* add a default body node to the block template node */
-      TConfigurationNode tBodyNode("body");
-      AddChildNode(m_tBlockTemplateNode, tBodyNode);
-      /* add a controller node to the block template node */
-      TConfigurationNode tControllerNode("controller");
-      SetNodeAttribute(tControllerNode, "config", "block");
-      AddChildNode(m_tBlockTemplateNode, tControllerNode);
-   }
+   CDISRoCSLoopFunctions::CDISRoCSLoopFunctions() {}
 
    /****************************************/
    /****************************************/
 
    void CDISRoCSLoopFunctions::Init(TConfigurationNode& t_tree) {
-      /* parse loop function configuration */
-      GetNodeAttribute(t_tree, "construction_site_min_corner", m_cConstructionSiteMinCorner);
-      GetNodeAttribute(t_tree, "construction_site_max_corner", m_cConstructionSiteMaxCorner);
-      GetNodeAttribute(t_tree, "blocks_in_foraging_area", m_unTargetBlocksInForagingArea);
-      /* calculate the corners of the arena */
-      const CVector3& cArenaSize(m_cSpace.GetArenaSize());
-      const CVector3& cArenaCenter(m_cSpace.GetArenaCenter());    
-      m_cArenaRangeX.Set(cArenaCenter.GetX() - (cArenaSize.GetX() * 0.5) + 0.1,
-                         cArenaCenter.GetX() + (cArenaSize.GetX() * 0.5) - 0.1);
-      m_cArenaRangeY.Set(cArenaCenter.GetY() - (cArenaSize.GetY() * 0.5) + 0.1,
-                         cArenaCenter.GetY() + (cArenaSize.GetY() * 0.5) - 0.1);
       /* create a map of the builderbot robots */
       using TValueType = std::pair<const std::string, CAny>;
-      for(const TValueType& t_robot : m_cSpace.GetEntitiesByType("builderbot")) {
-         m_vecRobots.push_back(any_cast<CBuilderBotEntity*>(t_robot.second));
+      try {
+         for(TValueType& t_robot : GetSpace().GetEntitiesByType("builderbot")) {
+            m_vecRobots.push_back(any_cast<CBuilderBotEntity*>(t_robot.second));
+         }
+      }
+      catch(CARGoSException &ex) {
+         LOGERR << "[WARNING] No BuilderBots added to the simulation at initialization." << std::endl;
       }
       /* create a map of stigmergic blocks */
-      for(const TValueType& t_block : m_cSpace.GetEntitiesByType("block")) {
-         m_vecBlocks.push_back(any_cast<CBlockEntity*>(t_block.second));
+      try {
+         for(TValueType& t_block : GetSpace().GetEntitiesByType("block")) {
+            m_vecBlocks.push_back(any_cast<CBlockEntity*>(t_block.second));
+         }
       }
+      catch(CARGoSException &ex) {
+         LOGERR << "[WARNING] No Stigmergic Blocks added to the simulation at initialization." << std::endl;
+      }
+      /* parse loop function configuration */
+      TConfigurationNodeIterator itCondition("condition");
+      for(itCondition = itCondition.begin(&t_tree);
+          itCondition != itCondition.end();
+          ++itCondition) {
+         /* parse the actions */
+         std::vector<std::shared_ptr<SAction> > vecActions;
+         TConfigurationNodeIterator itAction("action");
+         for(itAction = itAction.begin(&(*itCondition));
+             itAction != itAction.end();
+             ++itAction) {
+            std::string strActionType;
+            UInt32 unDelay = 0;
+            GetNodeAttribute(*itAction, "type", strActionType);
+            GetNodeAttributeOrDefault(*itAction, "delay", unDelay, unDelay);
+            if(strActionType == "add_timer") {
+               std::string strId;
+               GetNodeAttribute(*itAction, "id", strId);
+               std::shared_ptr<SAddTimerAction> ptrTimerAction =
+                  std::make_shared<SAddTimerAction>(*this, unDelay, std::move(strId));
+               vecActions.emplace_back(std::move(ptrTimerAction));
+            }
+            else if(strActionType == "add_entity") {
+               TConfigurationNodeIterator itEntity;
+               for(itEntity = itEntity.begin(&(*itAction));
+                   itEntity != itEntity.end();
+                   ++itEntity) {
+                  std::shared_ptr<SAddEntityAction> ptrEntityAction =
+                     std::make_shared<SAddEntityAction>(*this, unDelay, *itEntity);
+                  vecActions.emplace_back(std::move(ptrEntityAction));
+               }
+            }
+            else if(strActionType == "terminate") {
+               std::shared_ptr<STerminateAction> ptrTerminateAction =
+                  std::make_shared<STerminateAction>(*this, unDelay);
+               vecActions.emplace_back(std::move(ptrTerminateAction));
+            }
+            else {
+               THROW_ARGOSEXCEPTION("Loop function action type \"" << strActionType << "\" not implemented.");
+            }
+         }
+         std::string strConditionType;
+         bool bOnce = false;
+         GetNodeAttribute(*itCondition, "type", strConditionType);
+         GetNodeAttributeOrDefault(*itCondition, "once", bOnce, bOnce);
+         if(strConditionType == "entity") {
+            std::string strId;
+            CVector3 cPosition;
+            Real fThreshold;
+            GetNodeAttribute(*itCondition, "id", strId);
+            GetNodeAttribute(*itCondition, "position", cPosition);
+            GetNodeAttribute(*itCondition, "threshold", fThreshold);
+            std::unique_ptr<SEntityCondition> ptrEntityCondition =
+               std::make_unique<SEntityCondition>(*this, bOnce, std::move(vecActions), std::move(strId), cPosition, fThreshold);
+            m_vecConditions.emplace_back(std::move(ptrEntityCondition));
+         }
+         else if(strConditionType == "timer") {
+            std::string strId;
+            UInt32 unValue;
+            GetNodeAttribute(*itCondition, "id", strId);
+            GetNodeAttribute(*itCondition, "value", unValue);
+            std::unique_ptr<STimerCondition> ptrTimerCondition =
+               std::make_unique<STimerCondition>(*this, bOnce, std::move(vecActions), std::move(strId), unValue);
+            m_vecConditions.emplace_back(std::move(ptrTimerCondition));
+         }
+         else {
+            THROW_ARGOSEXCEPTION("Loop function condition type \"" << strConditionType << "\" not implemented.");
+         }
+      }
+   }
+
+   /****************************************/
+   /****************************************/
+
+   void CDISRoCSLoopFunctions::Reset() {
+      /* remove all dynamically added entities */
+      for(CEntity* pc_entity : m_vecAddedEntities) {
+         CallEntityOperation<CSpaceOperationRemoveEntity, CSpace, void>(GetSpace(), *pc_entity);
+      }
+      m_vecAddedEntities.clear();
+      /* clear is experiment finished */
+      m_bTerminate = false;
+      /* clear map of timers */
+      m_mapTimers.clear();
+      /* reenable all conditions */
+      for(std::unique_ptr<SCondition>& ptr_condition : m_vecConditions) {
+         ptr_condition->Enabled = true;
+      }
+      
    }
 
    /****************************************/
    /****************************************/
 
    void CDISRoCSLoopFunctions::PreStep() {
-      UInt32 unBlocksInConstructionSite = 0;
-      UInt32 unBlocksInForagingArea = 0;
-      /* loop over all the blocks */
-      for(CBlockEntity* pc_block : m_vecBlocks) {
-         SAnchor& sAnchor = pc_block->GetEmbodiedEntity().GetOriginAnchor();
-         if(sAnchor.Position.GetX() < m_cConstructionSiteMaxCorner.GetX() &&
-            sAnchor.Position.GetX() > m_cConstructionSiteMinCorner.GetX() &&
-            sAnchor.Position.GetY() < m_cConstructionSiteMaxCorner.GetY() &&
-            sAnchor.Position.GetY() > m_cConstructionSiteMinCorner.GetY()) {
-            unBlocksInConstructionSite++;
+      UInt32 unClock = GetSpace().GetSimulationClock();
+      /* increment all timers */
+      for(std::pair<const std::string, UInt32>& c_timer : m_mapTimers) {
+         std::get<UInt32>(c_timer)++;
+      }
+      /* check conditions */
+      for(std::unique_ptr<SCondition>& ptr_condition : m_vecConditions) {
+         if(ptr_condition->Enabled && ptr_condition->IsTrue()) {
+            /* schedule the associated actions */
+            for(const std::shared_ptr<SAction>& ptr_action : ptr_condition->Actions) {
+               m_mapPendingActions.emplace(unClock + ptr_action->Delay, ptr_action);
+            }
+            if(ptr_condition->Once) {
+               ptr_condition->Enabled = false;
+            }
+         }
+      }
+      /* execute actions for the current timestep */
+      std::pair<std::multimap<UInt32, std::shared_ptr<SAction> >::iterator, std::multimap<UInt32, std::shared_ptr<SAction> >::iterator> cActionRange =
+         m_mapPendingActions.equal_range(unClock);
+      for(std::multimap<UInt32, std::shared_ptr<SAction> >::iterator itAction = cActionRange.first;
+          itAction != cActionRange.second;
+          ++itAction) {
+         itAction->second->Execute();
+      }
+      m_mapPendingActions.erase(unClock);
+   }
+
+   /****************************************/
+   /****************************************/
+
+   void CDISRoCSLoopFunctions::PostStep() {    
+      
+   }
+
+   /****************************************/
+   /****************************************/
+
+   bool CDISRoCSLoopFunctions::IsExperimentFinished() {
+      return m_bTerminate;
+   }
+
+   /****************************************/
+   /****************************************/
+
+   bool CDISRoCSLoopFunctions::SEntityCondition::IsTrue() {
+      try {
+         CEntity& cEntity = Parent.GetSpace().GetEntity(EntityId);
+         CComposableEntity* pcComposableEntity =
+            dynamic_cast<CComposableEntity*>(&cEntity);
+         if(pcComposableEntity != nullptr && pcComposableEntity->HasComponent("body")) {
+            CEmbodiedEntity& cEmbodiedEntity =
+               pcComposableEntity->GetComponent<CEmbodiedEntity>("body");
+            Real fDistance = 
+               Distance(Position, cEmbodiedEntity.GetOriginAnchor().Position);
+            return (fDistance < Threshold);
+         }
+      }
+      catch(CARGoSException& ex) {}
+      /* if we reach this point the condition is false */
+      return false;
+   }
+
+   /****************************************/
+   /****************************************/
+
+   bool CDISRoCSLoopFunctions::STimerCondition::IsTrue() {
+      std::map<std::string, UInt32>::iterator itTimer =
+         Parent.m_mapTimers.find(TimerId);
+      if(itTimer != std::end(Parent.m_mapTimers)) {
+         return (itTimer->second == Value);
+      }
+      return false;
+   }
+
+   /****************************************/
+   /****************************************/
+
+   void CDISRoCSLoopFunctions::SAddEntityAction::Execute() {
+      CEntity* pcEntity = CFactory<CEntity>::New(Configuration.Value());
+      pcEntity->Init(Configuration);
+      CallEntityOperation<CSpaceOperationAddEntity, CSpace, void>(Parent.GetSpace(), *pcEntity);
+      /* attempt to do a collision check */
+      CComposableEntity* pcComposableEntity =
+         dynamic_cast<CComposableEntity*>(pcEntity);
+      if((pcComposableEntity != nullptr) && pcComposableEntity->HasComponent("body")) {
+         CEmbodiedEntity& cEmbodiedEntity =
+            pcComposableEntity->GetComponent<CEmbodiedEntity>("body");
+         if(cEmbodiedEntity.IsCollidingWithSomething()) {
+            LOGERR << "[WARNING] Failed to add entity \"" 
+                   << pcEntity->GetId()
+                   << "\" since it would have collided with something"
+                   << std::endl;
+            CallEntityOperation<CSpaceOperationRemoveEntity, CSpace, void>(Parent.GetSpace(), *pcEntity);
          }
          else {
-            unBlocksInForagingArea++;
+            /* entity added successfully */
+            Parent.m_vecAddedEntities.push_back(pcEntity);
+            return;
          }
       }
+      LOGERR << "[WARNING] Could not perform collision test for entity \""
+             << pcEntity->GetId()
+             << std::endl;
+   }
 
-      if(unBlocksInForagingArea < m_unTargetBlocksInForagingArea) {
-         for(;;) {
-            /* find a location for a block */
-            Real fX = CSimulator::GetInstance().GetRNG()->Uniform(m_cArenaRangeX);
-            Real fY = CSimulator::GetInstance().GetRNG()->Uniform(m_cArenaRangeY);
-            /* check that the location is not inside the construction site */
-            if(fX > m_cConstructionSiteMinCorner.GetX() &&
-               fX < m_cConstructionSiteMaxCorner.GetX() &&
-               fY > m_cConstructionSiteMinCorner.GetY() &&
-               fY < m_cConstructionSiteMaxCorner.GetY()) {
-               continue;
-            }
-            /* do not space items within 25 cm of another robot */
-            bool bTooClose = false;
-            for(CBuilderBotEntity* pc_robot : m_vecRobots) {
-               SAnchor& sAnchor = pc_robot->GetEmbodiedEntity().GetOriginAnchor();
-               if(Distance(sAnchor.Position, CVector3(fX, fY, 0.0)) < 0.25) {
-                  bTooClose = true;
-                  break;
-               }
-            }
-            if(bTooClose) {
-               continue;
-            }
-            /* attempt to place block and check for collisions */
-            TConfigurationNode& tBody = GetNode(m_tBlockTemplateNode, "body");
-            SetNodeAttribute(m_tBlockTemplateNode, "id", "block" + std::to_string(m_vecBlocks.size()));
-            SetNodeAttribute(tBody, "position", CVector3(fX, fY, 0.25));
-            //SetNodeAttribute(tBodyNode, "orientation", (*pcOrientationGenerator)(bRetry));
-            /* Create and initialized the block */
-            CBlockEntity* pcBlock = new CBlockEntity;
-            pcBlock->Init(m_tBlockTemplateNode);
-            CallEntityOperation<CSpaceOperationAddEntity, CSpace, void>(m_cSpace, *pcBlock);
-            /* Check if it's colliding with something */
-            if(pcBlock->GetEmbodiedEntity().IsCollidingWithSomething()) {
-               CallEntityOperation<CSpaceOperationRemoveEntity, CSpace, void>(m_cSpace, *pcBlock);
-               continue;
-            }
-            /* all checks passed */
-            m_vecBlocks.push_back(pcBlock);
-            break;
-         }
+   /****************************************/
+   /****************************************/
+
+   void CDISRoCSLoopFunctions::SAddTimerAction::Execute() {
+      if(Parent.m_mapTimers.count(TimerId) != 0) {
+         LOGERR << "[WARNING] Timer \""
+                << TimerId
+                << "\" already exists and has been reset to zero"
+                << std::endl;
       }
+      Parent.m_mapTimers[TimerId] = 0;
+   }
 
-      //std::cerr << "unBlocksInConstructionSite = " << static_cast<int>(unBlocksInConstructionSite) << std::endl;
-      //std::cerr << "unBlocksInForagingArea = " << static_cast<int>(unBlocksInForagingArea) << std::endl;
+   /****************************************/
+   /****************************************/
 
+   void CDISRoCSLoopFunctions::STerminateAction::Execute() {
+      Parent.m_bTerminate = true;
    }
 
    /****************************************/
--- src/loop_functions/di_srocs_loop_functions.h
+++ src/loop_functions/di_srocs_loop_functions.h
@@ -22,34 +22,113 @@ namespace argos {
 
       virtual ~CDISRoCSLoopFunctions() {}
 
-      virtual void Init(TConfigurationNode& t_tree);
+      virtual void Init(TConfigurationNode& t_tree) override;
 
-      //virtual void Reset();
+      virtual void Reset() override;
 
       //virtual void Destroy();
 
-      virtual void PreStep();
+      virtual void PreStep() override;
 
-      //virtual void PostStep();
+      virtual void PostStep() override;
+
+      virtual bool IsExperimentFinished() override;
 
    private:
-      CSpace& m_cSpace;
 
-      TConfigurationNode m_tBlockTemplateNode;
+      struct SAction {
+         SAction(CDISRoCSLoopFunctions& c_parent,
+                 UInt32 un_delay) :
+            Parent(c_parent),
+            Delay(un_delay) {}
+         virtual void Execute() = 0;
+         CDISRoCSLoopFunctions& Parent;
+         const UInt32 Delay = 0;
+      };
 
-      CRange<Real> m_cArenaRangeX;
-      CRange<Real> m_cArenaRangeY;
-      CRange<Real> m_cRotationRange;
+      struct SAddEntityAction : SAction {
+         SAddEntityAction(CDISRoCSLoopFunctions& c_parent,
+                          UInt32 un_delay,
+                          const TConfigurationNode& t_configuration) :
+            SAction(c_parent, un_delay),
+            Configuration(t_configuration) {}
+         virtual void Execute() override;
+         TConfigurationNode Configuration;
+      };
 
-      CVector2 m_cConstructionSiteMinCorner;
-      CVector2 m_cConstructionSiteMaxCorner;
+      struct SAddTimerAction : SAction {
+         SAddTimerAction(CDISRoCSLoopFunctions& c_parent,
+                         UInt32 un_delay,
+                         std::string&& str_timer_id) :
+            SAction(c_parent, un_delay),
+            TimerId(str_timer_id) {}
+         virtual void Execute() override;
+         std::string TimerId;
+      };
 
-      UInt32 m_unTargetBlocksInForagingArea;
+      struct STerminateAction : SAction {
+         STerminateAction(CDISRoCSLoopFunctions& c_parent,
+                          UInt32 un_delay) :
+            SAction(c_parent, un_delay) {}
+         virtual void Execute() override;
+      };
+
+      struct SCondition {
+         SCondition(CDISRoCSLoopFunctions& c_parent,
+                    bool b_once,
+                    std::vector<std::shared_ptr<SAction> >&& vec_actions) :
+            Parent(c_parent),
+            Once(b_once),
+            Enabled(true),
+            Actions(std::move(vec_actions)) {}
+         virtual bool IsTrue() = 0;
+         CDISRoCSLoopFunctions& Parent;
+         bool Once;
+         bool Enabled;
+         std::vector<std::shared_ptr<SAction> > Actions;
+      };
+
+      struct SEntityCondition : SCondition {
+         SEntityCondition(CDISRoCSLoopFunctions& c_parent,
+                          bool b_once,
+                          std::vector<std::shared_ptr<SAction> >&& vec_actions,
+                          std::string&& str_entity_id,
+                          const CVector3& c_position,
+                          Real f_threshold = 0.0f) :
+            SCondition(c_parent, b_once, std::move(vec_actions)),
+            EntityId(std::move(str_entity_id)),
+            Position(c_position),
+            Threshold(f_threshold) {}
+         virtual bool IsTrue() override;
+         std::string EntityId;
+         CVector3 Position;
+         Real Threshold;
+      };
+
+      struct STimerCondition : SCondition {
+         STimerCondition(CDISRoCSLoopFunctions& c_parent,
+                         bool b_once,
+                         std::vector<std::shared_ptr<SAction> >&& vec_actions,
+                         std::string&& str_timer_id,
+                         UInt32 un_value) :
+            SCondition(c_parent, b_once, std::move(vec_actions)),
+            TimerId(std::move(str_timer_id)),
+            Value(un_value) {}
+         virtual bool IsTrue() override;
+         std::string TimerId;
+         UInt32 Value;
+      };
+
+      std::vector<std::unique_ptr<SCondition> > m_vecConditions;
+      std::multimap<UInt32, std::shared_ptr<SAction> > m_mapPendingActions;
+      std::vector<CEntity*> m_vecAddedEntities;
 
       std::vector<CBuilderBotEntity*> m_vecRobots;
       std::vector<CBlockEntity*> m_vecBlocks;
 
+      std::map<std::string, UInt32> m_mapTimers;
 
+      bool m_bTerminate = false;
 
 
 
